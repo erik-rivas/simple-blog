e7847114508613757a6487f579f076534c594d74
erik-rivas <erikrivas.qro@gmail.com>
2023-11-29 12:59:06 -0600

    Added pikachu and charizard

---
+++ src/content/post/pokemon-card-pikachu.md
@@ -0,0 +1,37 @@
+---
+publishDate: 2023-11-30T00:00:00Z
+title: Pikachu Card
+excerpt: Pikachu is the best pokemon
+image: /pokemon/pikachu.png
+category: pokemon
+---
+
+
+## Pokémon Type
+- **Electric**
+
+## Description
+- **Brief**: Pikachu, the Electric Mouse Pokémon, is one of the most recognizable and beloved Pokémon worldwide. Known for its cute, cuddly appearance and powerful electric abilities, Pikachu is a staple in the Pokémon universe.
+
+- **Detailed**: Pikachu is famous for its bright yellow fur and long ears with black tips. Its cheeks can store electricity, which it releases during battles or when it feels threatened. Despite its small size, Pikachu is agile and quick, making it a formidable opponent in battles. Pikachu's signature move, Thunderbolt, is known for its high voltage and precision. Loyal and friendly, Pikachu develops strong bonds with its trainers and is known for its willingness to protect them at all costs.
+
+## Abilities
+- **Static**: May cause paralysis if touched.
+- **Lightning Rod** (Hidden Ability): Draws in all Electric-type moves to up its Sp. Atk.
+
+## Stats
+- **HP**: 35
+- **Attack**: 55
+- **Defense**: 40
+- **Special Attack**: 50
+- **Special Defense**: 50
+- **Speed**: 90
+
+## Evolutions
+- **Pre-Evolution**: Pichu
+- **Evolution**: Raichu (when exposed to a Thunder Stone)
+
+## Trivia
+- Pikachu is the most well-known Pokémon globally and serves as the franchise's official mascot.
+- It was one of the original 151 Pokémon introduced in Pokémon Red and Green (Blue in international releases).
+- Pikachu's design was conceived by Ken Sugimori and Atsuko Nishida.
--- src/navigation.js
+++ src/navigation.js
@@ -25,88 +25,24 @@ export const headerData = {
     },
     {
       text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
     },
     {
       text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
     },
     {
-      text: 'Blog',
+      text: 'Entries',
       links: [
         {
           text: 'Blog List',
           href: getBlogPermalink(),
         },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
+          text: 'AstroWind tutorials',
           href: getPermalink('tutorials', 'category'),
         },
         {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
+          text: 'Pokemon cards',
+          href: getPermalink('pokemon', 'category'),
         },
       ],
     },
@@ -115,7 +51,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [],
 };
 
 export const footerData = {
